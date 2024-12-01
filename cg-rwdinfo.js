9208a87229b191806813aca8e59813fd0f7b5ce8
highQ77 <highqualityliang.77@gmail.com>
2024-12-01 13:18:00 +0800

    update cg-rwdinfo.js

--- cg-rwdinfo.js
+++ cg-rwdinfo.js
@@ -9,11 +9,19 @@ export let showRWDInfo = (cg) => {
     // left bottom tips (rwd prefix name and breakpoint value)
     let s = ''
     let bpkeys = Object.keys(cg.breakpoints)
+    let range1 = [], range2 = []
     bpkeys.forEach(bp => {
+        range1.push(cg.breakpoints[bp])
+        range2.push(cg.breakpoints[bp])
+    })
+    range2.shift()
+    range2.push(10000)
+    range2 = range2.map(i => i - 1)
+    bpkeys.forEach((bp, idx) => {
         let bpmin = cg.breakpoints[bp];
         let str = `@media(min-width:${bpmin}px){`
         str += `body::before {
-                content:'${bp.toLocaleUpperCase()}-${bpmin}';
+                content:'${bp.toLocaleUpperCase()}-${range1[idx]}~${range2[idx]}';
                 position:fixed;bottom:0px;
                 background:rgba(0,0,0,.5);
                 color:white;
